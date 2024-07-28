a2fadec1be3e6531c323ef3979182be13b442606
nehagajja <nissyakhil@gmail.com>
2024-07-29 00:25:19 +0530

    codechange2

--- productmanage/prodapp/urls.py
+++ productmanage/prodapp/urls.py
@@ -1,7 +1,8 @@
 # urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet
+from .views import ProductViewSet, CategoryViewSet,export_products_csv,home_view
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -9,5 +10,9 @@ router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('forms/',home_view),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('export/products/csv/', export_products_csv, name='export_products_csv'),
 ]
 
--- productmanage/prodapp/views.py
+++ productmanage/prodapp/views.py
@@ -2,6 +2,10 @@
 from rest_framework import viewsets
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
+import csv
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.shortcuts import render
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -11,3 +15,22 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+def export_products_csv(request):
+    # Create the HttpResponse object with CSV header
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=products.csv'
+
+    writer = csv.writer(response)
+    writer.writerow(['ID', 'Name', 'Description', 'Price', 'Category','Status','Created_at','Expiry_Date','Uploaded_By'])
+
+    products = Product.objects.all()
+    for product in products:
+        writer.writerow([product.id, product.name, product.description, product.price, product.category.name,product.status,product.created_at,product.expiry_date,product.uploaded_by])
+
+    return response
+
+def home_view(request):
+    #if request.method == 'POST':
+        #result = generate_dummy_data.delay()
+        #return JsonResponse({'status': 'Task started', 'task_id': result.id})
+    return render(request, 'home.html')
